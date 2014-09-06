04dccc3699f7ef2306c03b1c29c867c2da666ce0
scapmo <scapelliti@gmail.com>
2014-09-06 14:28:35 -0400

    move h2 into header div

--- js/synthetorium.js
+++ js/synthetorium.js
@@ -3,7 +3,49 @@ $(function(){
 
 	/* Color Cycle - jQuery Plugin - v0.1.0 - created by: hello@jonathangrover.com */
 
-	$('a').colorCycle();
+	// $('a').colorCycle();
+
+/* Color Cycle - jQuery Plugin - v0.1.0 - created by: hello@jonathangrover.com */
+(function ($) {
+    
+    $.fn.colorCycle = function( options ) {
+        
+        var settings = $.extend({
+                 colors: ['#D3E2B6', '#C3DBB4', '#AACCB1', '#87BDB1', '#68B3AF', '#5B9FB4', '#63AABC', '#84BBD0', '#B0DEED', '#BEE1DD', ''],
+                 animationStartRange: 100,
+                 animationEndRange: 400,
+                 loopStartRange: 1000,
+                 loopEndRange: 4000
+            }, options),
+            $el = this,
+            generateTime = function (min, max) {
+                return Math.floor(Math.random() * (max - min)) + min;
+            },
+            selectColor = function () {
+                return settings.colors[Math.floor(Math.random() * settings.colors.length)];
+            },
+            changeColor = function (ele) {
+                ele.animate({
+                    'background-color': selectColor()
+                }, generateTime(settings.animationStartRange, settings.animationEndRange), function () {
+                    setTimeout(function () {
+                        changeColor(ele);
+                    }, generateTime(settings.loopStartRange, settings.loopEndRange));
+                });
+            },
+            initialize = function () {
+                for (i = 0; i < $el.size(); i++){
+                    changeColor($el.eq(i));
+                }
+            };
+        
+        initialize();
+        return $el;  
+    }
+})(jQuery);
+
+$('a').colorCycle();
+
 
 	var player = $('#player1')[0];
 
@@ -17,5 +59,7 @@ $(function(){
 		player.pause();
 		player.load();
 	});
+
+
 
 });
